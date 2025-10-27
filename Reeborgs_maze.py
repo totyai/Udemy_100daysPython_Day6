63c37f41f92101d06de14a2bc583945c9dcb0ae5
totyai <90376621+totyai@users.noreply.github.com>
2025-10-27 21:29:37 +0000

    - Finished Learning - Started daily challange

---
+++ Reeborgs_maze.py
@@ -0,0 +1,7 @@
+"""
+For this project, we are going to complete [Reeborg's Maze challange](https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json)
+Lost in a maze
+Reeborg was exploring a dark maze and the battery in its flashlight ran out.
+
+Write a program  so Reeborg can find the exit. THe map of the maze is the same each time, but the robot's location and facing direction is unknown.
+"""
