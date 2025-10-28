06324a3c9761c3ff63564b0f388b1020c3a8e93a
totyai <90376621+totyai@users.noreply.github.com>
2025-10-28 19:17:20 +0000

    - Finished Project Maze

--- Reeborgs_maze.py
+++ Reeborgs_maze.py
@@ -5,3 +5,16 @@ Reeborg was exploring a dark maze and the battery in its flashlight ran out.
 
 Write a program  so Reeborg can find the exit. THe map of the maze is the same each time, but the robot's location and facing direction is unknown.
 """
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        if front_is_clear():
+            move()
+    else:
+        turn_left()
